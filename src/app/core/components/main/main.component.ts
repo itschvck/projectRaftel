546bba09d5b9e8ecc414c5ed5a8fee1673e5b323
itschvck <gorospeee@gmail.com>
2022-05-26 00:52:20 +0200

    Added daisyUI and refactored navbar and hero on homepage. Added logic for navbar in MainComponent
    Signed-off-by: RMUCSC\sb002685 <d.gorospe@xview.it>

--- src/app/core/components/main/main.component.ts
+++ src/app/core/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'raftel-main',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      public r: Router
+  ) { }
 
   ngOnInit(): void {
   }
-
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,33 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkMidnightBlue : '#2c3e50',
+        lightMidnightBlue: '#34495e',
+        darkConcrete     : '#7f8c8d',
+        lightConcrete    : '#95a5a6',
+        darkClour        : '#bdc3c7',
+        lightCloud       : '#ecf0f1',
+        darkPomegranate  : '#c0392b',
+        lightPomegranate : '#e74c3c',
+        darkCarrot       : '#d35400',
+        normalCarrot     : '#e67e22',
+        lightCarrot      : '#f39c12',
+        normalSunflower  : '#f1c40f',
+        darkAqua         : '#16a085',
+        lightAqua        : '#1abc9c',
+        darkGrass        : '#27ae60',
+        lightGrass       : '#2ecc71',
+        darkSky          : '#2980b9',
+        lightSky         : '#3498db',
+        darkMina         : '#8e44ad',
+        lightMina        : '#9b59b6'
+
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
 }
